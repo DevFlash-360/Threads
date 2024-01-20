29b6dde6e94582a252be6fa8f9b8759b3fc4372c
DevFlashPlus <devflash360@gmail.com>
2024-01-21 05:31:42 +0900

    This branch is for auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+export default function RootLayout() {
+    return (
+        <html lang="en">
+            <body>
+                <h1>Auth page</h1>
+            </body>
+        </html>
+    )
+}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>Threads</h1>
-      <p>This is the onboarding page..</p>
+      <h1>Home</h1>
     </main>
   )
 }
