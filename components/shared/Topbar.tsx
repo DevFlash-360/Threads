ecb8d00144f4b78c0c4f93b5ab9955ed61ffa9a0
DevStair <finglord.468@gmail.com>
2024-02-08 00:58:35 +0900

    navigation bar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,4 +1,4 @@
-import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -13,10 +13,26 @@ function Topbar() {
             <div className="flex items-center gap-1">
                 <div className="block md:hidden"> 
                     <SignedIn>
-                        <SignOutButton></SignOutButton>
+                        <SignOutButton>
+                            <div className="flex cursor-pointer">
+                                <Image
+                                    src="/assets/logout.svg"
+                                    alt="logout"
+                                    width={24}
+                                    height={24}
+                                />
+                            </div>
+                        </SignOutButton>
                     </SignedIn>
                 </div>
 
+                <OrganizationSwitcher 
+                    appearance={{
+                        elements: {
+                            organizationSwitcherTrigger: "py-2 px-4"
+                        }
+                    }}
+                />
             </div>
         </nav>
     )
